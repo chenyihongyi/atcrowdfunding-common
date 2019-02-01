a092da7da9f6cd0359425482e97541ff15038276
chenyihong <chenyihongyi@163.com>
2019-02-01 23:11:28 +0800

    访问权限拦截器

--- src/main/java/com/atguigu/atcrowdfunding/util/Const.java
+++ src/main/java/com/atguigu/atcrowdfunding/util/Const.java
@@ -9,4 +9,7 @@ public class Const {
 
     public static final String LOGIN_USER = "user";
     public static final String PASSWORD = "123456";
+    public static final String  MY_URIS = "myURIs";
+    public static final String ALL_PERMISSION_URI = "allPermissionUris";
+
 }
